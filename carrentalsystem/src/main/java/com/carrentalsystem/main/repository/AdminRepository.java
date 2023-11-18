8394403ba30b68d2c1c48079ce29d277172d1d83
Deekshitha <deekshithas37@gmail.com>
2023-11-18 12:50:37 +0530

    removed code

--- carrentalsystem/src/main/java/com/carrentalsystem/main/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carrentalsystem.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.carrentalsystem.main.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
--- carrentalsystem/src/main/java/com/carrentalsystem/main/service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.carrentalsystem.main.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PaymentService {
-
-}
